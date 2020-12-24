7b4bdff00c0c779f842a2978820c55915261f6a5
pon3939 <ryuuos3939@gmail.com>
2020-12-24 16:22:29 +0900

    Prettier導入

--- index.js
+++ index.js
@@ -14,21 +14,24 @@ let mainWindow = null;
 
 // 全てのウィンドウが閉じたら終了
 app.on("window-all-closed", () => {
-	if (process.platform != "darwin") {
-		app.quit();
-	}
+  if (process.platform != "darwin") {
+    app.quit();
+  }
 });
-
 
 // Electronの初期化完了後に実行
 app.on("ready", () => {
-	//ウィンドウサイズを1280*720（フレームサイズを含まない）に設定する
-	mainWindow = new BrowserWindow({ width: 1280, height: 720, useContentSize: true });
-	//使用するhtmlファイルを指定する
-	mainWindow.loadURL(`file://${__dirname}/index.html`);
+  //ウィンドウサイズを1280*720（フレームサイズを含まない）に設定する
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    useContentSize: true,
+  });
+  //使用するhtmlファイルを指定する
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-	// ウィンドウが閉じられたらアプリも終了
-	mainWindow.on("closed", () => {
-		mainWindow = null;
-	});
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 });
