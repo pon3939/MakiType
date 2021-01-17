d7af239c4186134c1f5a2d0299ccf3834b74fd51
pon3939 <ryuuos3939@gmail.com>
2021-01-17 22:39:29 +0900

    gulpセットアップ

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const { dest, src, watch } = require('gulp');
+const sass = require('gulp-sass');
+
+const srcPath = 'src/';
+const scssPath = `${srcPath}scss/**/*.scss`;
+const cssPath = `${srcPath}css`;
+
+// Sass(Scss)ファイルをコンパイル
+const compileSass = (done) => {
+  src(scssPath)
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(dest(cssPath));
+  done();
+};
+
+// Sass(Scss)ファイルに変更があればコンパイル
+const watchSass = (done) => {
+  compileSass(done);
+  watch(scssPath, compileSass);
+  done();
+};
+
+exports.sass = compileSass;
+exports.watch = watchSass;
+exports.default = watchSass;
